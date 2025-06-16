3cba096a828d1318cdd1b01d734b5a0310879330
Caitlin Creighton <fallenoneart@gmail.com>
2025-06-16 14:54:35 -0700

    Rename folder from 'Csharp Video' to 'Csharp'

---
+++ server.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+console.log('Twitch Client ID:', process.env.TWITCH_CLIENT_ID);
+console.log('Twitch OAuth Token:', process.env.TWITCH_OAUTH);
+console.log('Twitch Username:', process.env.TWITCH_USERNAME);
+console.log('YouTube API Key:', process.env.YOUTUBE_API_KEY);
+console.log('YouTube Channel ID:', process.env.YOUTUBE_CHANNEL_ID);
+
+
